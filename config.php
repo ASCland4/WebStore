ba5b01f578c08a99a2d2c43a4dfbbf5c778ac86c
ASCland4 <94983399+ASCland4@users.noreply.github.com>
2022-04-26 20:57:57 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+define('APP_NAME',          'PHPSTORE');
+define('APP_VERSION',       '1.0.0');
+
+define('BASE_URL',          'http://localhost/WebStore/public/');
+
+// MYSQL
+define('MYSQL_SERVER',      'localhost');
+define('MYSQL_DATABASE',    'php_store');
+define('MYSQL_USER',        'user_php_store');
+define('MYSQL_PASS',        'XOn81awoD6C4');
+define('MYSQL_CHARSET',     'utf8');
+
+// MAIL
+define('EMAIL_HOST',        'smtp.gmail.com');
+define('EMAIL_FROM',        'aniel.lenno2@gmail.com');
+define('EMAIL_PASS',        '2Block40-');
+define('EMAIL_PORT',        465);
